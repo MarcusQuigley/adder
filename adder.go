4fde783296d2e9721f3cd843949e7d2765153cf5
marcusquigley <marcusquigley@gmail.com>
2026-07-09 16:28:56 +0100

    adding comments

--- adder.go
+++ adder.go
@@ -1,5 +1,13 @@
+// Paackage adder provides functionality
+// to add 2 numbers.
 package adder
 
+//Add adds to numbers
+//It has two parameters: two ints. Add returns the result
+//of the two numbers added.
+
+//
+// [Mathsisfun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
